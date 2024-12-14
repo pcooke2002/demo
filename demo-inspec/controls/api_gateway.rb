48a1a67faf6843c7cbf7db1a9d930e1ea6d4cbe7
peter cooke <pcooke2002@yahoo.com>
2024-12-14 07:56:31 -0800

    temp

--- demo-inspec/controls/api_gateway.rb
+++ demo-inspec/controls/api_gateway.rb
@@ -1,28 +1,18 @@
-# control 'aws-api-gateway-1' do
-#   impact 1.0
-#   title 'Ensure API Gateway is configured correctly'
-#   desc 'Check if the API Gateway has the correct settings'
-#
-#   aws_api_gateways.rest_api_ids.each do |rest_api_id|
-#     describe aws_api_gateway(rest_api_id: rest_api_id) do
-#       it { should exist }
-#       its('minimum_compression_size') { should be <= 0 }
-#       its('api_key_source') { should eq 'HEADER' }
-#     end
-#   end
-# end
-#
-# control 'aws-api-gateway-1' do
-#   impact 1.0
-#   title 'Ensure API Gateway is configured correctly'
-#   desc 'Check if the API Gateway has the correct settings'
-#
-#   aws_api_gateways.rest_api_ids.each do |rest_api_id|
-#     describe aws_api_gateway(rest_api_id: rest_api_id) do
-#       it { should exist }
-#       its('minimum_compression_size') { should be <= 0 }
-#       its('api_key_source') { should eq 'HEADER' }
-#       its('logging_level') { should eq 'INFO' }
-#     end
-#   end
-# end
+# controls/api_gateway.rb
+require 'pry'
+control 'api-gateway-exists' do
+  impact 1.0
+  title 'Ensure the API Gateway exists'
+  desc 'Validates that the specified API Gateway is present in the AWS account.'
+  describe aws_api_gateway_resource(rest_api_id: input('rest_api_gw_id'), resource_id: input('rest_gateway_resource_id')) do
+    it { should exist }
+    its('id') { should cmp input('rest_gateway_resource_id') }
+    its('path') { should_not be_nil }
+    its('parent_id') { should be_nil }
+    its('path_part') { should be_nil }
+    its('minimum_compression_size') { should be <= 0 }
+    its('resource_methods') { should should_not be_nil }
+
+#     binding.pry
+  end
+end
--- demo-inspec/controls/s3.rb
+++ demo-inspec/controls/s3.rb
@@ -5,10 +5,10 @@ control 'aws-s3-buckets' do
   title 'Ensure S3 buckets are configured correctly'
   desc 'Check if the S3 buckets have the correct settings'
 
-
   input('aws_s3_bucket_names').each do |bucket_name|
     describe aws_s3_bucket(bucket_name: bucket_name) do
         it { should exist }
+        #binding.pry
         its('bucket_acl') { should_not include('public-read') }
     end
   end
